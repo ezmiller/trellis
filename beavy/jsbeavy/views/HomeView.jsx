5ddd0e64abcaedba2bce81f38908d281c9e8b3df
Ethan Zane Miller <ethanzanemiller@gmail.com>
2015-12-26 21:59:46 +0100

    Add translations for HomeView

--- beavy/jsbeavy/views/HomeView.jsx
+++ beavy/jsbeavy/views/HomeView.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
+import i18n from 'utils/i18n'
+
+console.log({i18n:i18n})
 
 export class HomeView extends React.Component {
   render () {
+    let n = 1
     return (
       <div className='container text-center'>
         <img src='http://beavy.xyz/logos/logo.svg' alt='beavy logo' width='150' />
-        <h1>Wecome to Beavy!</h1>
+        <h1>{i18n.gettext('Welcome to Beavy!')}</h1>
         <p>
-        Please take a look at the <a href='https://beavyhq.gitbooks.io/beavy-documentation/content/' target='_blank'>documentation</a>.
+        {i18n.gettext('Please take a look at the')} <a href='https://beavyhq.gitbooks.io/beavy-documentation/content/' target='_blank'>{i18n.gettext('documentation')}</a>.
         </p>
       </div>
     )
