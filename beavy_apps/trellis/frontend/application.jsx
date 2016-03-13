a1999afc3aa11a8ae97bf79eb1e4a8f33c99aa7a
Ethan Zane Miller <ethanzanemiller@gmail.com>
2016-03-13 15:40:43 +0100

    Add base application.jsx file

---
+++ beavy_apps/trellis/frontend/application.jsx
@@ -0,0 +1,24 @@
+import React, { PropTypes } from 'react'
+import { MainMenu } from 'components/MainMenu'
+import UserModal from 'containers/UserModal'
+import UserMenuWidget from 'containers/UserMenuWidget'
+
+import { getExtensions } from 'config/extensions'
+
+export default class Application extends React.Component {
+  static propTypes = {
+    children: PropTypes.object
+  }
+  render () {
+    return <div>
+            <UserModal />
+            <MainMenu
+              logo='http://svgporn.com/logos/kong.svg'
+              navigationTools={<UserMenuWidget />}
+            >
+              {getExtensions('MainMenuItem').map(x => x.call(this))}
+            </MainMenu>
+            {this.props.children}
+          </div>
+  }
+}
