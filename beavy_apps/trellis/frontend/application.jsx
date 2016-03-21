4199d9d7274d902e20ef7927a8c74858aa2bcae8
Ethan Zane Miller <ethanzanemiller@gmail.com>
2016-03-21 22:22:17 +0000

    Merge remote-tracking branch 'upstream/master' into trellis
    # Conflicts:
    #	.travis.yml

--- beavy_apps/trellis/frontend/application.jsx
+++ beavy_apps/trellis/frontend/application.jsx
@@ -1,23 +1,26 @@
+import { React, PropTypes } from 'react'
+import { MainMenu } from 'components/MainMenu'
+import UserModal from 'containers/UserModal'
+import UserMenuWidget from 'containers/UserMenuWidget'
 
-import React from "react";
-import { MainMenu } from "components/MainMenu";
-import UserModal from "containers/UserModal";
-import UserMenuWidget from "containers/UserMenuWidget";
-
-import { getExtensions } from "config/extensions";
+import { getExtensions } from 'config/extensions'
 
 // This is your app entry point
 export default class Application extends React.Component {
-    render() {
-        return <div>
-                  <UserModal />
-                  <MainMenu
-                    logo='http://beavy.xyz/logos/logo.svg'
-                    navigationTools={<UserMenuWidget />}
-                  >
-                    {getExtensions('MainMenuItem').map(x=>x.call(this))}
-                  </MainMenu>
-                  {this.props.children}
-                </div>;
-    }
+  static propTypes = {
+    children: PropTypes.object
+  }
+
+  render () {
+    return <div>
+            <UserModal />
+              <MainMenu
+                logo='http://beavy.xyz/logos/kong.svg'
+                navigationTools={<UserMenuWidget />}
+              >
+                {getExtensions('MainMenuItem').map(x => x.call(this))}
+              </MainMenu>
+              {this.props.children}
+           </div>
+  }
 }
