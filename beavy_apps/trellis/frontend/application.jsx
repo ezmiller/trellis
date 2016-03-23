4f8cf5b2ec45c7bba1e7f82e64f02a3c1b8eb011
Ethan Zane Miller <ethanzanemiller@gmail.com>
2016-03-23 07:30:37 +0000

    Fix import syntax error

--- beavy_apps/trellis/frontend/application.jsx
+++ beavy_apps/trellis/frontend/application.jsx
@@ -1,4 +1,4 @@
-import { React, PropTypes } from 'react'
+import React, { PropTypes } from 'react'
 import { MainMenu } from 'components/MainMenu'
 import UserModal from 'containers/UserModal'
 import UserMenuWidget from 'containers/UserMenuWidget'
