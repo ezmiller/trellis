40196d80d726b9fecc4e36e5db6dd6acebf407b2
Ethan Zane Miller <ethanzanemiller@gmail.com>
2016-03-24 11:30:56 +0000

    Fix code style

--- beavy_apps/trellis/__init__.py
+++ beavy_apps/trellis/__init__.py
@@ -1,4 +1,4 @@
-from .models import Link, Topic
+# from .models import Link, Topic
 from .blueprint import trellis_bp
 from .models import *  # noqa
 from .views import *  # noqa
@@ -7,4 +7,3 @@ from .views import *  # noqa
 def init_app(app):
 
     app.register_blueprint(trellis_bp)
-
