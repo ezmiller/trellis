d0321951a4c2d88280f055c89d9b35dce870f569
Ethan Zane Miller <ethanzanemiller@gmail.com>
2016-03-24 12:03:04 +0000

    Add frontend files
    Pull request: #15
    Approved by: ezmiller

---
+++ beavy_apps/trellis/frontend/setup.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { addExtension, addNamedExtension } from 'config/extensions'
+import { Route } from 'react-router'
+import { STORY_SUBMIT, STORY_SUBMIT_REQUEST, STORY_SUBMIT_SUCCESS, STORY_SUBMIT_FAILURE } from './config/consts'
+
+import simpleSubmit from 'reducers/simple_submit'
+
+import SubmitView from './views/SubmitView'
+
+addNamedExtension('reducers', STORY_SUBMIT, simpleSubmit({
+  types: [ STORY_SUBMIT_REQUEST, STORY_SUBMIT_SUCCESS, STORY_SUBMIT_FAILURE ]
+}))
+
+export function setupViews (Application) {
+  addExtension('routes',
+    <Route key='submit' name='submit' path='/submit/' component={SubmitView} />)
+}
